7634a782705d1ee45d68631f3839a38a85088474
Brian Bowden <brian@bitstomp.com>
2014-09-25 02:30:47 -0400

    Fixing post to get for test

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 
-router.post('/locations/chattanooga', function(req, res) {
+router.get('/locations/chattanooga', function(req, res) {
     res.json({status: 'succcessful'});
     res.end();
 })
