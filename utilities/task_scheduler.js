c4b885f96c6d4a48b9bdd328878bdfdc65cef33b
Brian Bowden <brian@bitstomp.com>
2014-09-24 23:11:52 -0400

    Tweaking schedule times

--- utilities/task_scheduler.js
+++ utilities/task_scheduler.js
@@ -2,7 +2,7 @@ var tdotController = require('../controllers/tdot_controller');
 
 var startScheduledTasks = function() {
   
-  var tenMinutes = 1000 * 60 * 1;
+  var tenMinutes = 1000 * 60 * 10;
   setInterval(function() {
     tdotController.syncEventData(function(totalEntities) {
       if (totalEntities) {
