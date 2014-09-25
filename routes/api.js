c151c8c4634f2b7bd4404ea85fea210dcec5718c
Brian Bowden <brian@bitstomp.com>
2014-09-25 02:05:23 -0400

    Adding super basic route functionality for testing

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,8 @@ var router = express.Router();
 var mongoose = require('mongoose');
 
 router.post('/locations/chattanooga', function(req, res) {
-
+    res.json({status: 'succcessful'});
+    res.end();
 })
 
 module.exports = router;
